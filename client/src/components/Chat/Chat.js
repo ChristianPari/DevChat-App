6b203e7c0b4cdd13477f05e7216c93bc040507d3
Christian Pari <christianpari@outlook.com>
2021-03-13 22:03:38 -0500

    Finished basic css styling version, will work on version 2 with Bootsrap next

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,12 +5,14 @@ import './Chat.css';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
 import Messages from '../Messages/Messages';
+import TextContainer from '../TextContainer/TextContainer';
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost:5000';
@@ -31,8 +33,10 @@ const Chat = ({ location }) => {
     setName(name);
     setRoom(room);
 
-    socket.emit('join', { name, room }, () => {
-    
+    socket.emit('join', { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
     });
 
     return () => {
@@ -45,6 +49,10 @@ const Chat = ({ location }) => {
   useEffect(() => {
     socket.on('message', (message) => {
       setMessages([...messages, message]);
+    });
+
+    socket.on("roomData", ({users}) => {
+      setUsers(users);
     });
   }, [messages]);
 
@@ -63,6 +71,7 @@ const Chat = ({ location }) => {
         <Messages messages={messages} name={name}/>
         <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
       </div>
+      <TextContainer users={users}/>
     </div>
   )
 };
