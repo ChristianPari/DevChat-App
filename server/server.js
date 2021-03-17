cf2cbc8a759bed4acc56ac4156744a320e0bae79
Christian Pari <christianpari@outlook.com>
2021-03-17 18:45:07 -0400

    Changes for heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
@@ -12,6 +13,8 @@ const server = http.createServer(app);
 const io = socketio(server);
 socketHanlding(io);
 
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
 app.use(router);
 
 server.listen(PORT, () => console.log(`server has started on port: ${PORT}`));
