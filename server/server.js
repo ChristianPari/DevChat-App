2b92050740ed0c7603111b09bdfc797d11db4818
Christian Pari <christianpari@outlook.com>
2021-03-17 20:03:28 -0400

    trying to fix error with heroku

--- server/server.js
+++ server/server.js
@@ -1,21 +1,22 @@
 const path = require('path');
 const express = require('express');
 const socketio = require('socket.io');
-const app = express();
-const http = require('http').Server(app);
+const http = require('http');
 const socketHanlding = require('./socketHandling');
 
 const PORT = process.env.port || 5000;
 
 const router = require('./router');
 
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+  path: 'dev-chat-demo.herokuapp.com'
+});
+socketHanlding(io);
 
 const buildPath = path.join(__dirname, '..', 'build');
-app.use(router);
 app.use(express.static(buildPath));
-
-const server = http;
-const io = socketio(server);
-socketHanlding(io);
+app.use(router);
 
 server.listen(PORT, () => console.log(`server has started on port: ${PORT}`));
